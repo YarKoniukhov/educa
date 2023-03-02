ae7fdcdbadb27442f7db3316022e106a0115be12
yar.koniukhov <yar.koniukhov@gmail.com>
2023-03-02 23:50:37 +0200

    Deploy to Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
-web: gunicorn educa.wsgi --log-file
+web: gunicorn educa_main.wsgi:application --log-file - --log-level debug
+python manage.py collectstatic --noinput
+manage.py migrate
--- educa_main/settings.py
+++ educa_main/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['edua.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['educa.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
